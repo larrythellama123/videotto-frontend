9d81f05e909378b0c1f45b8427e4c52864de8d77
RohitK <rkurup01@gmail.com>
2026-01-30 00:59:44 +0800

    we

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const API_URL = 'https://videotto-frontend.xyz/process'  
+const API_URL = 'https://videotto-frontend.xyz'  
 
 
 function App() {
